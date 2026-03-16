8c6646b6d7ae3268f9a3dc793eb8a956de556bdf
zen4x <97395432+zen4xx@users.noreply.github.com>
2026-03-16 13:17:57 +0300

    Update math.h

--- src/math/math.h
+++ src/math/math.h
@@ -1,34 +1,21 @@
 #ifndef MATH_H
 #define MATH_H
 
-struct Vector2
-{
-    float x;
-    float y;
-};
-
-struct Vector3
-{
-    float x;
-    float y;
-    float z;
-};
-
-static struct Vector2 projectPoint(struct Vector3 p, struct Vector3 cam, int w, int h, float f)
+static Vector2 projectPoint(Vector3 p, Vector3 cam, int w, int h, float f)
 {
     // world -> camera
-    struct Vector3 pc = {
+    Vector3 pc = {
         p.x - cam.x,
         p.y - cam.y,
         p.z - cam.z};
 
     // perspective
-    struct Vector2 proj = {
+    Vector2 proj = {
         pc.x / pc.z,
         pc.y / pc.z};
 
     // screen
-    struct Vector2 screen = {
+    Vector2 screen = {
         proj.x * f + w / 2,
         -proj.y * f + h / 2};
 

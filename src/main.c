b85a92363481f1b44413a7000a58c5d82f45e8d5
zen4xx <yarikshcher@gmail.com>
2026-03-29 14:32:34 +0300

    added rotation

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "math/math.h" // raylib is also here
 #include "raylib.h"
+#include <stdio.h>
 
 int main(void)
 {
@@ -9,16 +10,33 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "EchoBreak Rasterization Tool");
     SetTargetFPS(60);
 
+    float pitch, yaw;
+    pitch = yaw = 0.f;
+    Vector3 cam = {0.f, 0.f, 0.0f};
+    float focalLength = 1200.f;
+
+    float cam_speed = 10.f;
+
+    float rotation_speed = 0.02f;
+    
     // Main
     while (!WindowShouldClose())
     {
         // Update
-        Vector3 cam = {0.f, 0.f, 0.0f};
-        float pitch, yaw = 0.f;
-        
-        Vector2 p1 = projectPoint((Vector3){0.f, 100.f, 3.f}, cam, yaw, pitch, screenWidth, screenHeight, 1.42);
-        Vector2 p2 = projectPoint((Vector3){-100.f, 0.f, 3.f}, cam, yaw, pitch, screenWidth, screenHeight, 1.42);
-        Vector2 p3 = projectPoint((Vector3){100.f, 0.f, 3.f}, cam, yaw, pitch, screenWidth, screenHeight, 1.42);
+
+        if (IsKeyDown(KEY_RIGHT)) yaw -= rotation_speed;
+        if (IsKeyDown(KEY_LEFT)) yaw += rotation_speed;
+        if (IsKeyDown(KEY_W)) cam.z += cam_speed;
+        if (IsKeyDown(KEY_A)) cam.x += cam_speed;
+        if (IsKeyDown(KEY_S)) cam.z -= cam_speed;
+        if (IsKeyDown(KEY_D)) cam.x -= cam_speed;
+
+        if (IsKeyDown(KEY_X)) { focalLength -= 1.f;  printf("focal length: %f\n", focalLength);}
+        if (IsKeyDown(KEY_Z)) { focalLength += 1.f;  printf("focal length: %f\n", focalLength);}
+
+        Vector2 p1 = projectPoint((Vector3){0.f, 100.f, 300.f}, cam, yaw, pitch, screenWidth, screenHeight, focalLength);
+        Vector2 p2 = projectPoint((Vector3){-100.f, 0.f, 300.f}, cam, yaw, pitch, screenWidth, screenHeight, focalLength);
+        Vector2 p3 = projectPoint((Vector3){100.f, 0.f, 300.f}, cam, yaw, pitch, screenWidth, screenHeight, focalLength);
             
 
         // Draw
